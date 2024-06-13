c0c48e1211fbe6a6159933db2166d14ac317abbd
João <joaovsaraivasousa@gmail.com>
2024-06-13 20:01:27 -0300

    feat: creating user class and setter for email and name

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: false
 
 class User
+  attr_accessor :email, :name
+
+  def set_email
+    puts "Enter your email"
+
+    self.email = $stdin.gets.chomp
+  end
+
+  def set_name
+    puts "Enter your name"
+
+    self.name = $stdin.gets.chomp
+  end
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'user'
+
+describe 'user' do
+  describe "#set_email" do 
+    it "should set email to instance variable based on user input`" do 
+      user = User.new
+      allow($stdin).to receive(:gets).and_return("johndoe@gmail.com\n")
+
+      expect { user.set_email }.to output("Enter your email\n").to_stdout
+      expect(user.email).to eq('johndoe@gmail.com')
+    end
+  end
+
+  describe "#set_name" do 
+  it "should set name to instance variable based on user input`" do 
+    user = User.new
+    allow($stdin).to receive(:gets).and_return("john doe\n")
+
+    expect { user.set_name }.to output("Enter your name\n").to_stdout
+    expect(user.name).to eq('john doe')
+  end
+end
+end
